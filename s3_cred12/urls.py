ff875eab2f14e1a637494a8388b9eb943ea64c41
veronny393 <veronny393@hotmail.com>
2024-12-31 11:33:10 -0500

    actualiza cred

--- s3_cred12/urls.py
+++ s3_cred12/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import index_s3_cred12, get_redes_s3_cred12, RptS3CredRed, RptCoberturaS3CredRed
+from .views import get_microredes_s3_cred12, p_microredes_s3_cred12, RptS3CredMicroRed
+from .views import get_establecimientos_s3_cred12, p_microredes_establec_s3_cred12, p_establecimientos_s3_cred12, RptS3CredEstablec
 
 
 urlpatterns = [
@@ -13,19 +15,21 @@ urlpatterns = [
     path('rpt_s3_cred12_excel/', RptS3CredRed.as_view(), name = 'rpt_s3_cred12_red_xls'),
     
     
+    # microredes
+    path('get_microredes_s3_cred12/<int:microredes_id>/', get_microredes_s3_cred12, name='get_microredes_s3_cred12'),
+    path('p_microredes_s3_cred12/', p_microredes_s3_cred12, name='p_microredes_s3_cred12'),
+    #-- microredes excel
+    path('rpt_s3_cred12_microred_excel/', RptS3CredMicroRed.as_view(), name = 'rpt_s3_cred12_red_xls'),
+    
+    # establecimientos
+    path('get_establecimientos_s3_cred12/<int:establecimiento_id>/', get_establecimientos_s3_cred12, name='get_establecimientos_s3_cred12'),
+    path('p_microredes_establec_s3_cred12/', p_microredes_establec_s3_cred12, name='p_microredes_establec_s3_cred12'),
+    path('p_establecimiento_s3_cred12/', p_establecimientos_s3_cred12, name='p_establecimientos_s3_cred12'),       
+    #-- estableccimiento excel
+    path('rpt_s3_cred12_establec_excel/', RptS3CredEstablec.as_view(), name = 'rpt_s3_cred12_red_xls'),
+        
     ### COBERTURA
     path('rpt_cobertura_s3_cred12_excel/', RptCoberturaS3CredRed.as_view(), name = 'rpt_cobertura_s3_cred12_xls'),
     
-    
-    
-    #microredes
-    # path('get_microredes/<int:microredes_id>/', views.get_microredes, name='get_microredes'),
-    # path('p_microredes/', views.p_microredes, name='p_microredes'),
-    # #-- microredes excel
-    # path('rpt_operacional_microred_excel/', RptOperacinalMicroRed.as_view(), name = 'rpt_operacional_microred_xls'),
-    # 
-    #establecimientos
-    # path('get_establecimientos/<int:establecimiento_id>/', views.get_establecimientos, name='get_establecimientos'),
-    # path('p_microredes_establec/', views.p_microredes_establec, name='p_microredes_establec'),
-    # path('p_establecimiento/', views.p_establecimientos, name='p_establecimientos'),    
+
 ]
